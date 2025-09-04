e627f926e0e27c8f47d403fc12be85df31b86825
Abdelati Elkateb <abdoelkateb.dev@gmail.com>
2025-09-04 20:28:35 +0200

    chore: file structure and config vite

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1 class="bg-red-900 text-red p-4 rounded-xl">
+          hiiiiiiiiii
+        </h1>
+        <v-btn color="primary">Hello Vuetify</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
 
 <style scoped>
 .logo {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+
+
+
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(vuetify).mount('#app')
