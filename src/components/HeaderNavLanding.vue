e0b3e8711a1c0ba9b096003654ec804b4346c3c6
Abdelati Elkateb <abdoelkateb.dev@gmail.com>
2025-09-08 15:28:14 +0200

    feat(navbar): add responsive dropdown menu

--- src/components/HeaderNavLanding.vue
+++ src/components/HeaderNavLanding.vue
@@ -1,29 +1,50 @@
 <template>
-  <header class="d-flex justify-space-around align-end px-6 py-3 bg-red h-[100px]">
-    
+  <!-- الـ Navbar -->
+  <v-app-bar class="h-[100px] mt-auto" color="green darken-4" >
     <!-- الشعار -->
-    <div>
-      <img :src="logo" alt="Logo" height="40" />
+    <v-app-bar-title>
+      <img :src="logo" alt="Logo" />
+    </v-app-bar-title>
+
+    <!-- الروابط (Desktop فقط) -->
+    <div class="d-none d-md-flex justify-center flex-grow-1">
+      <v-btn text>Home</v-btn>
+      <v-btn text>Shop</v-btn>
+      <v-btn text>About</v-btn>
+      <v-btn text>Contact</v-btn>
     </div>
 
-    <!-- روابط -->
-    <nav class="d-none d-md-flex gap-6">
-      <v-btn variant="text" class="text-white" v-for="item in menuItems" :key="item">
-        {{ item }}
+    <!-- الأيقونات -->
+    <template v-slot:append>
+      <!-- زر Menu يظهر في الموبايل -->
+      <v-btn icon class="d-md-none" @click="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
       </v-btn>
-    </nav>
 
-    <!-- أيقونات -->
-    <div class="d-flex gap-3 align-center">
-      <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
-      <v-btn icon><v-icon>mdi-heart</v-icon></v-btn>
-      <v-btn icon><v-icon>mdi-cart-minus</v-icon></v-btn>
-    </div>
+      <!-- أيقونات تظهر في Desktop -->
+      <div class="d-none d-md-flex gap-2">
+        <v-btn icon="mdi-account"></v-btn>
+        <v-btn icon="mdi-magnify"></v-btn>
+        <v-btn icon="mdi-heart"></v-btn>
+        <v-btn icon="mdi-cart-minus"></v-btn>
+      </div>
+    </template>
+  </v-app-bar>
 
-  </header>
+  <!-- Drawer للموبايل -->
+  <v-navigation-drawer v-model="drawer" temporary location="left">
+    <v-list>
+      <v-list-item v-for="item in menuItems" :key="item" @click="drawer = false">
+        <v-list-item-title>{{ item }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue"
 import logo from "../assets/images/logo_cropped.png"
-const menuItems = ['Home', 'Shop', 'About', 'Contact']
+
+const drawer = ref(false)
+const menuItems = ["Home", "Shop", "About", "Contact"]
 </script>
